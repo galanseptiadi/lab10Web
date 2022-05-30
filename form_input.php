bdbb96b6ffb9bb2f176174c9f831f8719b5fe5f7
galanseptiadi <101730390+galanseptiadi@users.noreply.github.com>
2022-05-30 22:52:57 +0700

    Add files via upload

---
+++ form_input.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Program memanfaatkan program 10.2 untuk membuat form inputan sederhana.
+ **/
+
+ include "form.php";
+
+ echo "<html><head><title>Mahasiswa</title></head><body>";
+ $form = new Form("","Input Form");
+ $form->addField("txtnim", "Nim");
+ $form->addField("txtnama", "Nama");
+ $form->addField("txtalamat", "Alamat");
+ echo "<h3>Silahakan isi form berikut ini :</h3>";
+ $form->displayForm();
+ echo "</body></html>";
+ ?>
